88c8bb00657ccc7055cc278ea0437515c316d369
Khanhpear97 <khanhpear97@gmail.com>
2024-01-09 09:06:55 +0700

    first commit

---
+++ src/main/java/com/example/th041701/Th041701Application.java
@@ -0,0 +1,13 @@
+package com.example.th041701;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Th041701Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Th041701Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/th041701/Th041701ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.th041701;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Th041701ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
